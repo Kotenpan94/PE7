dc53dc2c15b3c2b9afe3de177bb7d2b36810d1d4
Kotenpan94 <59397435+Kotenpan94@users.noreply.github.com>
2020-09-03 16:40:05 -0400

    File Upload :0

---
+++ PE07GoldsteenMahala/PE07GoldsteenMahala/PE07GoldsteenMahala/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PE07GoldsteenMahala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
